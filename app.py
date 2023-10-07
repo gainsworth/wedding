7fc2b78b5d079a411979a4e383a1cdce70610242
owner <darknesscrazyman@hotmail.com>
2023-10-07 20:30:41 +0100

    requirements.txt

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+# # Configure SQLite database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# db = SQLAlchemy(app)
 
 
-# Create a model for RSVPs
-class RSVP(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+# # Create a model for RSVPs
+# class RSVP(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(100), nullable=False)
 
 
 # Define routes and views
@@ -27,9 +27,9 @@ def rsvp():
         name = request.form['name']
         email = request.form['email']
 
-        rsvp_entry = RSVP(name=name, email=email)
-        db.session.add(rsvp_entry)
-        db.session.commit()
+        # rsvp_entry = RSVP(name=name, email=email)
+        # db.session.add(rsvp_entry)
+        # db.session.commit()
 
         return redirect(url_for('home'))
 
@@ -37,6 +37,6 @@ def rsvp():
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
